6e50405267d208a9e40f90ac64337632de0da4e3
RoosterQMonee <82356323+RoosterQMonee@users.noreply.github.com>
2022-01-22 17:35:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import sys
 from cx_Freeze import setup, Executable
 
-include_files = ['autorun.inf']
+include_files = []
 base = None
 
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(name="MiniClient2",
-      version="0.1",
+setup(name="MiniClient",
+      version="1.0",
       description="A small client",
       options={'build_exe':{'include_files':include_files}},
       executables=[Executable("miniclient.py", base=base)])
